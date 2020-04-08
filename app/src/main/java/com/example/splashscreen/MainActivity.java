8b2cd4ae99fdf1ba8a0918c4d33657146166dd60
axiftaj <47206155+axiftaj@users.noreply.github.com>
2020-04-09 00:32:04 +0500

    started
    a

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 2000; //2000 means 2 second
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               startActivity(new Intent(MainActivity.this , HomeActivity.class));
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
